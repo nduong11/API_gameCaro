2b3526cdfacac5bb622614bacf801db8f0d7aa74
nduong11 <nongduong0811@gmail.com>
2025-01-15 17:43:07 +0700

    sa

--- models/modelRank.js
+++ models/modelRank.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const modelsRanks = new mongoose.Schema({
-    userame: {
+    username: {
         type: String,
         required: true
     },
